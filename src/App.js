d834c42aa03d94c2900b7fdad02935f4fd40341a
LusineTorosian <lusinetorosyan.evistep@gmail.com>
2022-10-04 14:07:28 +0400

    about validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const[message, setMessage] = useState("");
+
+  const emailValidation = () => {
+    const regEx = /[a-zA-Z0-9._%+-] + @[a-z0-9-]+\.[a-z]{2,8}|(.[a-z{}])?/g
+    if(regEx.test(email)) {
+      setMessage("Email is vaild")
+    } else if (!regEx.test(email) && email != 0) {
+      setMessage("Email is not valid");
+    } else {
+      setMessage("");
+    }
+  }
+
+  const onChangeHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <h2> Email validation</h2>
+      </div>
+      <div className="card">
+        <label htmlFor="email" className="label">
+          Email:
+        </label>
+        <input 
+          id="email"
+          className="input"
+          type="email"
+          placeholder="email" 
+          value={email}
+          onChange={onChangeHandler} 
+        />
+        <button onClick={emailValidation}>Check</button>
+        <p className="message">{message}</p>
+      </div>
     </div>
   );
 }
